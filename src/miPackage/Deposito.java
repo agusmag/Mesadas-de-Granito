d9ab9d7ba75130a3687b16a16a36067e908beccc
Agustin Magliano <agustinmagliano@gmail.com>
2017-08-27 18:47:55 -0300

    Version Terminada 2.0

--- src/miPackage/Deposito.java
+++ src/miPackage/Deposito.java
@@ -2,6 +2,9 @@ package miPackage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Locale;
@@ -9,45 +12,49 @@ import java.util.Scanner;
 
 public class Deposito {
 	private LinkedList<Mesada> listaMesa;
-	
-	
-	public Deposito(String path) throws FileNotFoundException{
+
+	public Deposito(String path) throws FileNotFoundException {
 		this.listaMesa = new LinkedList<Mesada>();
 		Scanner sc = new Scanner(new File(path));
 		sc.useLocale(Locale.ENGLISH);
 		int cant = sc.nextInt();
-		
+
 		for (int i = 0; i < cant; i++) {
 			listaMesa.add(new Mesada(sc.nextDouble(), sc.nextDouble()));
 		}
-			
-		
+
 		sc.close();
 	}
-	
-	public void Mostrar(){
+
+	public void Mostrar() {
 		for (Mesada mesada : listaMesa) {
 			System.out.println(mesada);
 		}
 	}
 	
-	public void Ordenar(){
+	public void Resolver(String pathS) throws IOException{
+		PrintWriter pw = new PrintWriter(new FileWriter(pathS));
+		pw.print(this.Apilar());
+		pw.close();
+	}
+
+	public void Ordenar() {
 		Collections.sort(this.listaMesa);
 	}
-	
-	public int Apilar(){
+
+	public int Apilar() {
 		int cantidadPilas = 0;
 		Mesada base;
+		int cant;
 		
-		
-		while(this.listaMesa.size() > 1){
+		while (this.listaMesa.size() > 1) {
 			base = this.listaMesa.removeFirst();
-			int cant = this.listaMesa.size();
-			
+			cant = this.listaMesa.size();
+
 			for (int i = 0; i < cant; i++) {
 				Mesada mesada = this.listaMesa.get(i);
-				
-				if (mesada.EntraEn(base)){
+
+				if (mesada.EntraEn(base)) {
 					base = mesada;
 					this.listaMesa.remove(i);
 					i--;
@@ -56,7 +63,7 @@ public class Deposito {
 			}
 			cantidadPilas++;
 		}
-		
-		return (this.listaMesa.size() == 1)? ++cantidadPilas : cantidadPilas;
+
+		return (this.listaMesa.size() == 1) ? ++cantidadPilas : cantidadPilas;
 	}
 }
--- src/miPackage/Main.java
+++ src/miPackage/Main.java
@@ -1,17 +1,19 @@
 package miPackage;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
-	
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		Deposito depo = new Deposito("./IN/Lote3.txt");
-		
+
+	public static void main(String[] args) throws IOException {
+
+		Deposito depo = new Deposito("./IN/Lote2.txt");
+
 		depo.Ordenar();
 		depo.Mostrar();
+		depo.Resolver("./OUT/Lote2.txt");
+
 		
-		System.out.println("La cantidad de pilas son:" + depo.Apilar());
+		//System.out.println("La cantidad de pilas son:" + depo.Apilar());
 	}
 
 }
