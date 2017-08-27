be86fe1126d1f28861d900f9ca484b129ecdee13
Agustin Magliano <agustinmagliano@gmail.com>
2017-08-27 16:00:04 -0300

    Clase y Constructor

--- src/miPackage/Main.java
+++ src/miPackage/Main.java
@@ -1,11 +1,24 @@
 package miPackage;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class Main {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		String miPath = "IN/LoteBase.txt";
+		Scanner sc = new Scanner(new File(miPath));
+		sc.useLocale(Locale.ENGLISH);
+		Mesada [] vec = new Mesada[sc.nextInt()];
 		
+		for (int i = 0; i < vec.length; i++) {
+			vec[i] = new Mesada(sc.nextDouble(), sc.nextDouble());
+			System.out.println(vec[i].getMedidas());
+		}
 		
-		
+		sc.close();
 	}
 
 }
--- src/miPackage/Mesada.java
+++ src/miPackage/Mesada.java
@@ -5,5 +5,14 @@ public class Mesada {
 	private double ancho;
 	
 	
+	public Mesada(final double largo, final double ancho){
+		this.largo = largo;
+		this.ancho = ancho;
+	}
+	
+	public String getMedidas(){
+		String medida = this.largo + "x" + this.ancho;
+		return medida;
+	}
 	
 }
